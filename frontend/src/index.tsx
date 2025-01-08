3f8ea29e406e144e1863f85cf4eba3147534619a
alexdelapaz <alexander.s.delapaz@gmail.com>
2025-01-08 14:48:02 -0500

    modified version of https://github.com/microsoft/sample-app-aoai-chatGPT to incorporate rules of system use acceptance

---
+++ frontend/src/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import { initializeIcons } from '@fluentui/react'
+
+import Chat from './pages/chat/Chat'
+import Layout from './pages/layout/Layout'
+import NoPage from './pages/NoPage'
+import { AppStateProvider } from './state/AppProvider'
+
+import {MessageDialog} from './components/Utilities/MessageDialog'
+
+import './index.css'
+
+initializeIcons()
+
+export default function App() {
+  return (
+    <AppStateProvider>
+
+      <MessageDialog/>
+
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Chat />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </AppStateProvider>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
